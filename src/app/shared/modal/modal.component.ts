9888cb325c3d766012696bc7f845caf58b2e2cc5
Dusan <dusan@buddyboss.com>
2022-09-05 11:54:35 +0200

    Create tabs for login

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  @Input() modalID = ''
+
+  constructor(
+       public modal: ModalService,
+       public el: ElementRef
+    ) { 
+ 
+  }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
+  }
+
+  closeModal(){
+     this.modal.toggleModal(this.modalID);
   }
 
 }
